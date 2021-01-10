2c8e52afdf368998911176ef10969c66b928c6fb
harikrishnan12345 <48360517+harikrishnan12345@users.noreply.github.com>
2021-01-11 03:13:05 +0530

    first comit

---
+++ src/main/java/com/coderulagam/movies/MovieLisApplication.java
@@ -0,0 +1,13 @@
+package com.coderulagam.movies;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieLisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovieLisApplication.class, args);
+	}
+
+}
